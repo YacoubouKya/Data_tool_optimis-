617ad0f2aaf1aa82e555fa1b1f4629fa0a66c372
YacoubouKya <yacouboukya93@gmail.com>
2026-02-04 09:16:39 +0100

    Refactor EDA script with improved profiling and correlation

--- eda.py
+++ eda.py
@@ -42,7 +42,7 @@ def generate_profile(df: pd.DataFrame):
         #)
     #else:
         # Profiling minimal pour petits datasets
-    profile = ProfileReport(df, title="Profiling EDA", minimal=True)
+    profile = ProfileReport(df, title="Profiling EDA")
     
     return profile
 
@@ -163,4 +163,5 @@ def run_eda(df: pd.DataFrame):
             st.pyplot(fig)
 
             plt.close(fig)
+
 
